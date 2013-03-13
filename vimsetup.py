ebf5a46680e6cef649a9f929cac5610b78d40910
Denver Coneybeare <denver.coneybeare@gmail.com>
2013-03-13 11:46:24 -0400

    vimsetup.py added

---
+++ vimsetup.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import io
+import os
+import sys
+import urllib2
+
+###############################################################################
+
+def main():
+    app = VimSetup()
+    app.run()
+    return 0
+
+###############################################################################
+
+class VimSetup(object):
+
+    def __init__(self, log_func=print):
+        self.log_func = log_func
+
+    def run(self):
+        src_dir_path = os.path.abspath(".")
+        dest_dir_path = os.path.expanduser("~")
+        self.create_rc_files(src_dir_path, dest_dir_path)
+        autoload_dir_path = self.create_autoload_dir(dest_dir_path)
+        self.install_pathogen(autoload_dir_path)
+        bundle_dir_path = self.create_bundle_dir(dest_dir_path)
+        self.install_tomorrow_theme(bundle_dir_path)
+
+    def create_rc_files(self, src_dir_path, dest_dir_path):
+        for filename in (".vimrc", ".gvimrc"):
+            dest_path = os.path.join(dest_dir_path, filename)
+            src_path = os.path.join(src_dir_path, filename)
+            self.log("Creating {}".format(dest_path))
+            with io.open(dest_path, "wt", encoding="utf-8") as f:
+                print("source {}".format(src_path), file=f)
+
+    def create_autoload_dir(self, dest_dir_path):
+        path = os.path.join(dest_dir_path, "vimfiles", "autoload")
+        self.mkdir(path)
+        return path
+
+    def install_pathogen(self, autoload_dir_path):
+        self.log("Installing pathogen.vim")
+        pathogen_vim_path = os.path.join(autoload_dir_path, "pathogen.vim")
+        pathogen_vim_url = "https://raw.github.com/tpope/vim-pathogen/" \
+            "master/autoload/pathogen.vim"
+        self.download_file(pathogen_vim_url, pathogen_vim_path)
+
+    def download_file(self, url, dest_path):
+        self.log("Downloading {} to {}".format(url, dest_path))
+        infile = urllib2.urlopen(url)
+        try:
+            with io.open(dest_path, "wb") as outfile:
+                while True:
+                    buf = infile.read(2048)
+                    if len(buf) == 0:
+                        break
+                    outfile.write(buf)
+        finally:
+            infile.close()
+
+    def create_bundle_dir(self, dest_dir_path):
+        path = os.path.join(dest_dir_path, "vimfiles", "bundle")
+        self.mkdir(path)
+        return path
+
+    def install_tomorrow_theme(self, bundle_dir_path):
+        self.log("Installing Tomorrow Night color scheme")
+        install_dir_path = os.path.join(bundle_dir_path, "vim-tomorrow-theme",
+            "colors")
+        self.mkdir(install_dir_path)
+        vim_url = "https://raw.github.com/chriskempson/tomorrow-theme/" \
+            "master/vim/colors/Tomorrow-Night-Bright.vim"
+        vim_path = os.path.join(install_dir_path, "Tomorrow-Night-Bright.vim")
+        self.download_file(vim_url, vim_path)
+
+    def mkdir(self, path):
+        self.log("Creating directory: {}".format(path))
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
+    def log(self, message):
+        log_func = self.log_func
+        if log_func is not None:
+            log_func(message)
+
+###############################################################################
+
+if __name__ == "__main__":
+    exit_code = main()
+    sys.exit(exit_code)
