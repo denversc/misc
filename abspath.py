2079be847701c240b62e7f25766e20f9b73a1456
Denver Coneybeare <denver@motorola.com>
2014-09-08 11:29:33 -0400

    abspath.py: support reading paths from stdin

--- abspath.py
+++ abspath.py
@@ -6,7 +6,23 @@ from __future__ import unicode_literals
 import os
 import sys
 
-for path in sys.argv[1:]:
-    abs_path = os.path.normpath(os.path.abspath(path))
-    print(abs_path)
+def paths_from_stdin_iter():
+    for line in sys.stdin:
+        yield line.rstrip()
 
+def main():
+    relative_paths = sys.argv[1:]
+    if len(relative_paths) > 0:
+        relative_paths_iter = relative_paths
+    else:
+        relative_paths_iter = paths_from_stdin_iter()
+
+    for path in relative_paths_iter:
+        abs_path = os.path.normpath(os.path.abspath(path))
+        print(abs_path)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
