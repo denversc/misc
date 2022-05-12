7e42714015bb918406d05b2ec5545e7550572d90
Denver Coneybeare <dconeybe@google.com>
2022-05-12 15:37:26 -0400

    abspath.py: convert to python3

--- abspath.py
+++ abspath.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python
-
-from __future__ import print_function
-from __future__ import unicode_literals
+#!/usr/bin/env python3
 
 import os
 import sys
 
-def paths_from_stdin_iter():
-    for line in sys.stdin:
-        yield line.rstrip()
-
 def main():
-    relative_paths = sys.argv[1:]
-    if len(relative_paths) > 0:
-        relative_paths_iter = relative_paths
-    else:
-        relative_paths_iter = paths_from_stdin_iter()
+  relative_paths = sys.argv[1:]
+  if len(relative_paths) > 0:
+    relative_paths_iter = relative_paths
+  else:
+    relative_paths_iter = (line.rstrip() for line in sys.stdin)
 
-    for path in relative_paths_iter:
-        abs_path = os.path.normpath(os.path.abspath(path))
-        print(abs_path)
+  for path in relative_paths_iter:
+    abs_path = os.path.normpath(os.path.abspath(path))
+    print(abs_path)
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        sys.exit(1)
+  main()
