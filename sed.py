aeee4ffc6aee112d510980688d3070796023b978
Denver Coneybeare <dconeybe@google.com>
2023-09-28 11:13:35 -0400

    sed.py: add -u argument to toggle ignoring utf8 decoding errors

--- sed.py
+++ sed.py
@@ -29,6 +29,7 @@ from __future__ import annotations
 
 from collections.abc import Sequence
 import dataclasses
+import enum
 import glob
 import pathlib
 import re
@@ -51,6 +52,15 @@ FLAG_OUTPUT_FILE = flags.DEFINE_string(
   The file to which to write the filtered results rather that to standard output.
   The directory of this file will _not_ be created automatically.
   The empty string can be specified to explicitly indicate to write to standard output.
+  """,
+)
+
+FLAG_SKIP_UTF8_DECODING_ERRORS = flags.DEFINE_boolean(
+    "u",
+    False,
+    """
+  If enabled, if UTF-8 decoding of a file fails, then just skip it.
+  If disabled (the default), fail the entire program if UTF-8 decoding of a file fails.
   """,
 )
 
@@ -92,16 +102,29 @@ def main(argv: Sequence[str]) -> None:
       for input_file_str in glob.iglob(input_file_pattern, recursive=True):
         input_file = pathlib.Path(input_file_str)
         if not input_file.is_file():
-          logging.debug("Skipping non-file: %s", input_file)
+          logging.debug("Skipping %s because it is not a file", input_file)
           continue
 
-        logging.debug("Reading input from %s", input_file)
+        logging.debug("Reading %s", input_file)
         with input_file.open("rt", encoding="utf8") as f:
-          input_text = f.read()
+          try:
+            input_text = f.read()
+          except UnicodeDecodeError as e:
+            if args.utf8_decode_error_handle_strategy == Utf8DecodeErrorHandleStrategy.FAIL:
+              print(f"ERROR: UTF-8 decoding of {input_file} failed: {e}")
+              return 1
+            if args.utf8_decode_error_handle_strategy == Utf8DecodeErrorHandleStrategy.SKIP:
+              logging.warning("Skipping %s because UTF-8 decoding failed (%s)", input_file, e)
+              continue
+            else:
+              raise RuntimeError(
+                  "INTERNAL ERROR: unknown value of args.utf8_decode_error_handle_strategy: "
+                  f"{args.utf8_decode_error_handle_strategy}"
+              )
 
         output_text = search_expr.sub(args.replacement_pattern, input_text)
         if output_text == input_text:
-          logging.debug("Pattern not found in %s; skipping", input_file)
+          logging.debug("Skipping %s because the pattern was not found", input_file)
           continue
 
         print_output_text(
@@ -111,12 +134,18 @@ def main(argv: Sequence[str]) -> None:
         )
 
 
+class Utf8DecodeErrorHandleStrategy(enum.Enum):
+  FAIL = enum.auto()
+  SKIP = enum.auto()
+
+
 @dataclasses.dataclass(frozen=True)
 class CommandLineArguments:
   search_pattern: str
   replacement_pattern: str
   input_file_patterns: tuple[str, ...]
   output_dest: pathlib.Path | Literal[INPLACE, STDOUT]
+  utf8_decode_error_handle_strategy: Utf8DecodeErrorHandleStrategy
 
 
 class CommandLineArgumentsParseError(Exception):
@@ -146,11 +175,17 @@ def parseCommandLineArguments(argv: Sequence[str]) -> CommandLineArguments:
   else:
     output_dest = STDOUT
 
+  if FLAG_SKIP_UTF8_DECODING_ERRORS.value:
+    utf8_decode_error_handle_strategy = Utf8DecodeErrorHandleStrategy.SKIP
+  else:
+    utf8_decode_error_handle_strategy = Utf8DecodeErrorHandleStrategy.FAIL
+
   return CommandLineArguments(
       search_pattern=search_pattern,
       replacement_pattern=replacement_pattern,
       input_file_patterns=tuple(argv[3:]),
       output_dest=output_dest,
+      utf8_decode_error_handle_strategy=utf8_decode_error_handle_strategy,
   )
 
 
@@ -160,14 +195,12 @@ def print_output_text(
   if dest is STDOUT or (dest is INPLACE and src is None):
     logging.debug("Writing result to standard output")
     sys.stdout.write(text)
-  elif dest is INPLACE:
-    logging.debug("Writing result to %s", src)
-    with src.open("wt", encoding="utf8") as f:
-      f.write(text)
-  else:
-    logging.debug("Writing result to %s", dest)
-    with dest.open("wt", encoding="utf8") as f:
-      f.write(text)
+    return
+
+  dest_file = src if dest is INPLACE else dest
+  logging.debug("Writing result to %s", dest_file)
+  with dest_file.open("wt", encoding="utf8") as f:
+    f.write(text)
 
 
 if __name__ == "__main__":
