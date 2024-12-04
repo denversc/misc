e1700c0e660a47ff2265d3e59fe9e1b61c2d5557
Denver Coneybeare <dconeybe@google.com>
2024-12-04 13:39:39 -0500

    nvim_git_editor.lua: add \f and \d key mappings.

--- nvim_git_editor.lua
+++ nvim_git_editor.lua
@@ -10,7 +10,8 @@ vim.opt_local.number = false
 -- Extract the filenames from the default git commit template's message and
 -- put them, one per line, at the top of the file. This makes it easy to include
 -- filenames in the git commit message, which is often desirable.
-do
+function insert_filenames_from_git_comment_comments()
+  local saved_cursor = vim.api.nvim_win_get_cursor(0)
   local pattern = "^#.-:%s*(.-)%s*$"
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
   local filenames = {}
@@ -23,7 +24,31 @@ do
   end
 
   if #filenames then
-    vim.api.nvim_buf_set_lines(0, 0, 0, false, filenames)
-    vim.api.nvim_win_set_cursor(0, {1, 0})
+    local insert_line = saved_cursor[1] - 1
+    vim.api.nvim_buf_set_lines(0, insert_line, insert_line, false, filenames)
+    vim.api.nvim_win_set_cursor(0, saved_cursor)
   end
+end
+
+-- When called, runs "git diff --cached" and appends the output to
+-- the current buffer.
+function git_diff_cached_append()
+  local handle = io.popen("git diff --cached")
+  while true do
+    local line = handle:read("*l")
+    if not line then
+      break
+    end
+    vim.api.nvim_buf_set_lines(0, -1, -1, false, {line})
+  end
+  handle:close()
+end
+
+-- Set up custom key mappings that are convenient when editing git commit messages.
+do
+  local buf_id = vim.api.nvim_get_current_buf()
+  local map = vim.api.nvim_buf_set_keymap
+  local map_options = { noremap = true, silent = true }
+  map(buf_id, 'n', '<Leader>d', '<Cmd>lua git_diff_cached_append()<CR>', map_options)
+  map(buf_id, 'n', '<Leader>f', '<Cmd>lua insert_filenames_from_git_comment_comments()<CR>', map_options)
 end
