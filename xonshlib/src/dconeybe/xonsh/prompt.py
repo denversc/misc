decf898a1b78d39606f77da8084f465e3a61ecb4
Denver Coneybeare <dconeybe@google.com>
2025-01-16 00:21:51 -0500

    reorg xonshrc

---
+++ xonshlib/src/dconeybe/xonsh/prompt.py
@@ -0,0 +1,33 @@
+###############################################################################
+# xonsh prompt setup
+# https://xon.sh/tutorial.html#customizing-the-prompt
+###############################################################################
+
+class Prompt:
+
+  def __init__(self, hostname: str) -> None:
+    self.hostname = hostname
+
+  def time_format(self) -> str:
+    # See https://docs.python.org/3.13/library/time.html#time.strftime
+    return "%a %b %d, %Y %H:%M:%S"
+
+  def prompt(self) -> str:
+    return (
+      "{BACKGROUND_" + self._hostname_color() + "}{WHITE}{user}@{hostname}{RESET} "
+      "{BOLD_YELLOW}{cwd}{RESET}"
+      "{BOLD_WHITE}{curr_branch: [git_branch={}]}{RESET}"
+      "{RED}{last_return_code_if_nonzero: [retcode={BOLD_INTENSE_RED}{}{RED}]{RESET}}"
+      "\n{BOLD_BLUE}{prompt_end}{RESET} "
+    )
+
+  def right_prompt(self) -> str:
+    return "{FAINT_WHITE}[{localtime}]"
+
+  def _hostname_color(self):
+    colors = {
+      "dconeybe2.c.googlers.com": "GREEN",
+      "dconeybe-macbookpro3.roam.internal": "BLUE"
+    }
+    return colors.get(self.hostname, "RED")
+
--- xonshrc.d/xontrib.xsh
+++
@@ -1,1 +0,0 @@
-xontrib load coreutils
