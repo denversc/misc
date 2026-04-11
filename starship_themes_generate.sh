43be9a5718c545bbb2a26a7591389213c19b945b
denver <github.prefix924@passmail.com>
2026-04-11 19:31:12 -0400

    starship.toml: simplify prompt and use tokyonight theme

--- starship_themes_generate.sh
+++ starship_themes_generate.sh
@@ -5,4 +5,4 @@ set -v
 
 echo "# Created by $0" >starship.green.toml
 echo >>starship.green.toml
-sed 's/#a3aed2/#a3d2a5/g; s/#769ff0/#76f08a/g; s/#394260/#396041/g; s/#212736/#213624/g; s/#1d2230/#213624/g; s/#a0a9cb/#aed2a3/g; s/fg:#e3e5e5 bg:#76f08a/fg:#090c0c bg:#76f08a/g' starship.toml >> starship.green.toml
+sed 's/#a3aed2/#a3d2a5/g; s/#769ff0/#76f08a/g; s/fg:#e3e5e5 bg:#76f08a/fg:#090c0c bg:#76f08a/g' starship.toml >> starship.green.toml
