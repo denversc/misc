6d96fdeed71265df7eec93b58ee9b555e72573ef
Denver Coneybeare <dconeybe@google.com>
2026-04-11 19:51:31 -0400

    starship.green.toml: use penguin instead of apple

--- starship_themes_generate.sh
+++ starship_themes_generate.sh
@@ -5,4 +5,4 @@ set -v
 
 echo "# Created by $0" >starship.green.toml
 echo >>starship.green.toml
-sed 's/#a3aed2/#a3d2a5/g; s/#769ff0/#76f08a/g; s/fg:#e3e5e5 bg:#76f08a/fg:#090c0c bg:#76f08a/g' starship.toml >> starship.green.toml
+sed 's///g; s/#a3aed2/#a3d2a5/g; s/#769ff0/#76f08a/g; s/fg:#e3e5e5 bg:#76f08a/fg:#090c0c bg:#76f08a/g' starship.toml >> starship.green.toml
