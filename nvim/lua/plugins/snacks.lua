515175da33c600dcef44f1eeef7d022930e93ac0
denver <github.prefix924@passmail.com>
2026-04-15 19:37:18 -0400

    nvim/lua/plugins/snacks.lua: change "gr" keymap to "gx" to avoid conflicting with neovim's builtin LSP keymaps
    See https://neovim.io/doc/user/lsp/#gra

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -86,7 +86,8 @@ return {
     -- LSP
     { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
     { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+    -- NOTE: "gr" changed to "gx" to avoid conflicting with neovim's builtin LSP keymaps: https://neovim.io/doc/user/lsp/#gra
+    { "gx", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
     { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
     { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
     { "gai", function() Snacks.picker.lsp_incoming_calls() end, desc = "C[a]lls Incoming" },
