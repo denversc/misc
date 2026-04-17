105532e712dc2c01de0192ef3d36b152e927607c
denver <github.prefix924@passmail.com>
2026-04-17 11:25:29 -0400

    gcloud_podman_macbook.zsh: change mounts

--- gcloud_podman_macbook.zsh
+++ gcloud_podman_macbook.zsh
@@ -3,9 +3,9 @@
 set -e -u -o pipefail
 
 exec podman run -it \
+  -v /Volumes/dev/gcloud-podman/dev01-493607-71c679891ef4.json:/dev01-493607-71c679891ef4.json:Z \
   -v /Volumes/dev/gcloud-podman/config:/root/.config/gcloud:Z \
-  -v /Volumes/dev/gcloud-podman/dev01-493607-71c679891ef4.json:/root/dev01-493607-71c679891ef4.json:Z \
-  -v /Volumes/dev/gcloud-podman/misc:/root/misc:Z \
+  -v /Volumes/dev/gcloud-podman/.bash_history:/root/.bash_history:Z \
   gcr.io/google.com/cloudsdktool/google-cloud-cli:alpine \
   /bin/bash
 
